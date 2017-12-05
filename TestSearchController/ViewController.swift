4d89cec62331acd26a833da42447c4d2ddc30817
Dong <donly@magicalboy.com>
2017-12-05 15:42:27 +0800

    fixed the bug from iOS 11

---
+++ TestSearchController/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  TestSearchController
+//
+//  Created by ERU on 2017/12/05.
+//  Copyright © 2017年 ERU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Demo"
+        
+//        definesPresentationContext = true
+//        extendedLayoutIncludesOpaqueBars = true
+//        navigationController?.hidesBarsWhenVerticallyCompact = true
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        navigationController?.barHideOnTapGestureRecognizer.addTarget(self, action: #selector(barHideAction(_:)))
+        navigationItem.searchController = searchController
+        navigationController?.hidesBarsOnTap = true
+        
+        // For debug
+        self.view.backgroundColor = UIColor.cyan
+        self.navigationController?.navigationBar.barTintColor = UIColor.yellow
+    }
+
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super .viewWillTransition(to: size, with: coordinator)
+        
+        if let nc = navigationController, nc.isNavigationBarHidden {
+            if size.width > size.height {
+                searchController.searchBar.isHidden =  size.width > size.height
+            }
+        }
+    }
+    
+    @objc func barHideAction(_ guesture: UITapGestureRecognizer) {
+        if let nc = navigationController {
+            let isHidden = nc.isNavigationBarHidden
+            searchController.searchBar.isHidden = isHidden
+//            nc.setNavigationBarHidden(!isHidden, animated: true)
+        }
+    }
+}
